d1071efdbe99750260fcaf75eadce65aa682bb0c
dilantha95 <hdilantha95@gmail.com>
2017-04-20 07:43:53 +0530

    Main Window added

---
+++ InventoryManagement/MainWindow.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace PharmacyManagement.InventoryManagement
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(53, 45);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(187, 52);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Products";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(53, 103);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(187, 52);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Stocks";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(53, 219);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(187, 52);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Reports";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(53, 161);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(187, 52);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Search Product";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(287, 316);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "MainWindow";
+            this.Text = "Inventory";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
